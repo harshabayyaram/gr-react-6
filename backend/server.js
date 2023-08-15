0c11ff3683748ddbbf9bd5d9a0eccfd16593e6b5
harsha <bayyaramharsha@gmail.com>
2023-08-15 13:36:54 +0530

    comments added

--- backend/server.js
+++ backend/server.js
@@ -8,19 +8,19 @@ const port = 3001;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-// Use the cors middleware to allow requests from any origin
+//allow requests from any origin
 app.use(cors());
 
 app.get("/test", (req, res) => {
-  console.log("Received GET request:", req.query);
-  res.json({ message: "GET request received successfully" });
+  console.log("Received GET request:", req.query);//console
+  res.json({ message: "GET request received successfully" });//page
 });
 
 app.post("/test1", (req, res) => {
-  console.log("Received POST request:", req.body);
-  res.json({ message: "POST request received successfully" });
+  console.log("Received POST request:", req.body);//console
+  res.json({ message: "POST request received successfully" });//page
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);//console starting port number
 });
